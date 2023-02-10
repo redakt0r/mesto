413ca2f0a81ab7258f2aae9eaeee1305e9b753fc
Mikhail Koryakin <sdrr87@gmail.com>
2023-02-10 18:00:50 +0300

    bugfix and refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editPopup = document.querySelector(".popup");
 
+const profileName = document.querySelector(".profile__name");
+const profileOccupation = document.querySelector(".profile__occupation");
+
 editProfileButton.addEventListener("click", function () {
   editPopup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  occupationInput.value = profileOccupation.textContent;
 });
 
 const closePopup = document.querySelector(".popup__close-button");
@@ -11,16 +16,17 @@ closePopup.addEventListener("click", function () {
   editPopup.classList.remove("popup_opened");
 });
 
-const nameInput = document.querySelector(".popup__name-field");
-const occupationInput = document.querySelector(".popup__occupation-field");
-
-const profileName = document.querySelector(".profile__name");
-const profileOccupation = document.querySelector(".profile__occupation");
-
-nameInput.value = profileName.textContent;
-occupationInput.value = profileOccupation.textContent;
-
 const formElement = document.querySelector(".popup__container");
+const nameInput = formElement.querySelector(".popup__input_name_name");
+const occupationInput = formElement.querySelector(
+  ".popup__input_name_occupation"
+);
+
+/* const profileName = document.querySelector(".profile__name");
+const profileOccupation = document.querySelector(".profile__occupation"); */
+
+/* nameInput.value = profileName.textContent;
+occupationInput.value = profileOccupation.textContent; */
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
